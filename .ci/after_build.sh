9938cd2d9df7aca978c5e554c75a242aaa3900d4
Admiral-Fish <admiralfish420@gmail.com>
2021-11-02 18:31:51 -0700

    Update paths

--- .ci/after_build.sh
+++ .ci/after_build.sh
@@ -3,13 +3,13 @@
 case $OS in
   linux)
   {
-    mv build/Source/Forms/Gen4ID .
+    mv build/Release/Gen4ID .
     zip -r Gen4ID-linux.zip Gen4ID
     sha256sum Gen4ID-linux.zip > Gen4ID-linux.zip.sha256
   } ;;  
   macOS)
   {
-    mv build/Source/Forms/Gen4ID.app .
+    mv build/Release/Gen4ID.app .
     PATH=$PATH:$HOME/Qt/6.1/macos/bin macdeployqt Gen4ID.app -dmg -verbose=2
     zip -r Gen4ID-macOS.zip Gen4ID.dmg
     shasum -a 256 Gen4ID-macOS.zip > Gen4ID-macOS.zip.sha256
