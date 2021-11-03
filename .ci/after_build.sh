e22fc69225dbbbd83bb308b6daebe112c06aefcb
Admiral-Fish <admiralfish420@gmail.com>
2021-11-02 18:42:23 -0700

    Correct path

--- .ci/after_build.sh
+++ .ci/after_build.sh
@@ -3,13 +3,13 @@
 case $OS in
   linux)
   {
-    mv build/Release/Gen4IDs .
+    mv build/Gen4IDs .
     zip -r Gen4IDs-linux.zip Gen4IDs
     sha256sum Gen4IDs-linux.zip > Gen4IDs-linux.zip.sha256
   } ;;  
   macOS)
   {
-    mv build/Release/Gen4IDs.app .
+    mv build/Gen4IDs.app .
     PATH=$PATH:$HOME/Qt/6.1/macos/bin macdeployqt Gen4IDs.app -dmg -verbose=2
     zip -r Gen4IDs-macOS.zip Gen4IDs.dmg
     shasum -a 256 Gen4IDs-macOS.zip > Gen4IDs-macOS.zip.sha256
